1a55c86da9b5e55854d10606d7b2e1d293727682
Salik <msalik32@gmail.com>
2021-06-19 15:17:34 +0530

    Matched all the changed fields from Student Schema in students routes files

--- routes/api/students.js
+++ routes/api/students.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const moment = require("moment");
 const { verifyToken } = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 const Student = require("../../models/student");
+
+// add regex to name
+// add validation to email
+// create a folder for validation
 
 // @route  POST api/students
 // @desc   Create a student
 // @access Private
 
-// create a folder for validation
 router.post(
   "/create-student",
+  verifyToken,
   [
     check("name", "Name is Required").not().isEmpty(),
-    check("phone", "phone is required").isNumeric(),
-    check("enrollmentStatus", "status of enrollment is required").isBoolean(),
+    check("phone", "phone is required").isMobilePhone(),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const { name, email, phone, enrollmentStatus } = req.body;
+    const { name, email, phone, enquiryStatus, enrollmentStatus } = req.body;
     try {
-      // See if student exists
-      let student = await Student.findOne({ phone });
-      if (student) {
-        res.status(400).json({ errors: [{ msg: "Student already exists" }] });
+      let studentPhone = await Student.findOne({ phone });
+      if (studentPhone) {
+        res.status(400).send("Student already exists");
       }
 
       if (!req.body.email) {
@@ -34,19 +37,25 @@ router.post(
           name,
           phone,
           enrollmentStatus,
+          enquiryStatus,
         });
       } else {
+        // create student by email
+        let studentEmail = await Student.findOne({ email });
+        if (studentEmail) {
+          res.status(400).send("Student already exists");
+        }
         student = new Student({
           name,
           email,
           phone,
           enrollmentStatus,
+          enquiryStatus,
         });
       }
       student.save();
     } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server error");
+      res.status(500).send(err.message);
     }
   }
 );
@@ -83,56 +92,7 @@ router.get("/list-students", verifyToken, async (req, res) => {
     });
     res.json(students);
   } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-// @route  POST api/students
-// @desc   get list of all the enrolled students
-// @access Private
-
-router.get("/list-enrolled-students", verifyToken, async (req, res) => {
-  try {
-    const query = { enrollmentStatus: true };
-    const enrolledStudents = await Student.find(
-      query,
-      (err, enrolledStudents) => {
-        if (!enrolledStudents) {
-          return res.status(404).json({
-            msg: "No enrolled students",
-          });
-        }
-      }
-    );
-    res.json(enrolledStudents);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-// @route  POST api/students
-// @desc   get all the enrolled students enrollment status
-// @access Private
-
-router.get("/list-enquiry-students", verifyToken, async (req, res) => {
-  try {
-    const query = { enrollmentStatus: false };
-    const enquiredStudents = await Student.find(
-      query,
-      (err, enquiredStudents) => {
-        if (!enquiredStudents) {
-          return res.status(404).json({
-            msg: "No enolled students",
-          });
-        }
-      }
-    );
-    res.json(enquiredStudents);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send(err.message);
   }
 });
 
@@ -142,7 +102,15 @@ router.get("/list-enquiry-students", verifyToken, async (req, res) => {
 
 router.put("/update-student/:phone", verifyToken, async (req, res) => {
   try {
-    const { name, email, phone, year } = req.body;
+    const {
+      name,
+      email,
+      phone,
+      enquiryStatus,
+      enrollmentStatus,
+      enquiryDate,
+      enrollmentDate,
+    } = req.body;
     const student = await Student.findOne({ phone: req.params.phone });
 
     if (!student) {
@@ -152,12 +120,14 @@ router.put("/update-student/:phone", verifyToken, async (req, res) => {
     if (name) student.name = name;
     if (email) student.email = email;
     if (phone) student.phone = phone;
-    if (year) student.year = year;
+    if (enquiryStatus) student.enquiryStatus = enquiryStatus;
+    if (enrollmentStatus) student.enrollmentStatus = enrollmentStatus;
+    if (enquiryDate) student.enquiryDate = enquiryDate;
+    if (enrollmentDate) student.enrollmentDate = enrollmentDate;
 
     res.json({ msg: `Student data got updated` });
   } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send(err.message);
   }
 });
 
@@ -178,8 +148,7 @@ router.delete("/delete-student/:phone", verifyToken, async (req, res) => {
       }
     );
   } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send(err.message);
   }
 });
 
