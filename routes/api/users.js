a52221c94dae4083d3a6ce6ab69599c58b94db3c
Salik <msalik32@gmail.com>
2021-07-07 13:03:59 +0530

    Moved logic to controller and cleaned the code

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
 const router = express.Router();
-const brcypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const User = require("../../Models/user");
+const { SignUp } = require("../../controllers/userController");
 
 // @route  POST api/users
 // @desc   Test route
@@ -12,88 +8,6 @@ const User = require("../../Models/user");
 
 // add regrex for username and password
 
-router.post(
-  "/sign-up",
-  [
-    check("name")
-      .not()
-      .isEmpty()
-      .withMessage("Please enter your name")
-      .isLength({ min: 2 })
-      .withMessage("User's name should atleast be 2 letter long"),
-    check("email")
-      .not()
-      .isEmpty()
-      .withMessage("Please enter your email address")
-      .isEmail()
-      .withMessage("Please enter a valid email address"),
-    check("password")
-      .not()
-      .isEmpty()
-      .withMessage("Please enter your password")
-      .isLength({ min: 8 })
-      .withMessage("Password must be atleast 8 character long"),
-    //.matches('/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/'),
-    check("phoneNumber")
-      .not()
-      .isEmpty()
-      .withMessage("Please enter your phone number")
-      .isNumeric()
-      .isLength({ min: 10, max: 10 })
-      .withMessage("Please enter a valid phone number"),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(406).send(errors.array({ onlyFirstError: true }));
-    }
-
-    const { name, email, password, phoneNumber } = req.body;
-    try {
-      //See if user exists
-      let userEmail = await User.findOne({ email });
-      let userPhoneNumber = await User.findOne({ phoneNumber });
-      if (userEmail) {
-        return res.status(502).send("The entered email is already registered");
-      }
-      if (userPhoneNumber) {
-        return res
-          .status(502)
-          .send("The entered phone number is already registered");
-      }
-
-      user = new User({
-        name,
-        email,
-        password,
-        phoneNumber,
-      });
-
-      //Encrypt password
-      const salt = await brcypt.genSalt(10);
-      user.password = await brcypt.hash(password, salt);
-      await user.save();
-
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.status(200).send("Registration Successful");
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server error");
-    }
-  }
-);
+router.post("/sign-up", SignUp);
 
 module.exports = router;
